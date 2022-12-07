480ca93fb1dfa552631f93d95e470fec8c420d16
verma076 <nvmasai0706@gmail.com>
2022-12-08 00:50:15 +0530

    done

---
+++ practice.js
@@ -0,0 +1,29 @@
+let num=13
+let count=0
+for(i=0;i<=num;i++){
+if(num%i==0){
+count++
+}
+}
+if(count==2){
+console.log("Prime")
+}
+else{
+console.log("Not Prime")
+}
+
+
+
+
+let str=masai
+let rev=""
+for(i-str.length-1;i>=0;i--){
+rev+=str[i]
+}
+if(rev==str){
+console.log("Palindrome")
+}
+else{
+console.log("Not Palimdrome"){
+}
+
